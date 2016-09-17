27c407c46e7e8539f19eb406e0aa7db57b0ff38d
Brandon Chong Chan Yean <brandon@brandhex.com>
2016-09-18 02:39:02 +0800

    Initial webbyCMS setup & commit.

---
+++ controller/define.php
@@ -0,0 +1,49 @@
+<?php
+$row="";$webTitle="WebbyCMS";
+$systemError="";$systemSucces="";
+if(empty($_SESSION["systemError"])){
+	$_SESSION["systemError"]="";
+}
+if(empty($_SESSION["systemSucces"])){
+	$_SESSION["systemSucces"]="";
+}
+$page="";
+if(isset($_GET["page"])&&!empty($_GET["page"])){
+	$page=$_GET["page"];
+}
+if($page==""){
+	$page="home";
+}
+$cate="";
+if(isset($_GET["cate"])&&!empty($_GET["cate"])){
+	$cate=$_GET["cate"];
+}
+$action="";
+if(isset($_GET["action"])&&!empty($_GET["action"])){
+	$action=$_GET["action"];
+}
+$id="";
+if(isset($_GET["id"])&&!empty($_GET["id"])){
+	$id=$_GET["id"];
+}
+$form="";
+if(isset($_POST["form"])&&!empty($_POST["form"])){
+	$form=$_POST["form"];
+}
+$token="";
+if(isset($_POST["token"])&&!empty($_POST["token"])){
+	$token=$_POST["token"];
+}
+
+/* Pre Defined Variables for Date & Time - This will used for Token generation */
+$Today=date("Y-m-d");
+$Day=date("d");
+$Month=date("m");
+$Year=date("Y");
+$Hour=date("g");
+$Minute=date("i");
+$Seconds=date("s");
+$Time=date("g:i A");
+
+$Token=md5($Today."webbycms".uniqid().$Time);
+?>
---
+++ includes/htmlstart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?=$webTitle?> | Bootstraps</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <!-- Bootstraps 4-alpha -->
+  <link href="/assets/bootstraps/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Webby Custom CSS -->
+  <link rel="stylesheet" href="/assets/css/custom.css">
+  <!-- Webby Custom Javascripts -->
+  <script src="/assets/js/custom.js"></script>
+</head>
