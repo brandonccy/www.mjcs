b34a37b47f04b64a313eaad8efe855bc19ac92db
Brandon Chong Chan Yean <brandon@brandhex.com>
2016-09-22 16:05:26 +0800

    Updates.

--- controller/define.php
+++ controller/define.php
@@ -47,4 +47,105 @@ $Seconds=date("s");
 $Time=date("g:i A");
 
 $Token=md5($Today."webbycms".uniqid().$Time);
+
+$emailBodyTemplate='
+<center style="width: 100%; background: #FFF;">
+<!-- Visually Hidden Preheader Text : BEGIN -->
+<div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
+    (Optional) This text will appear in the inbox preview, but not the email body.
+</div>
+<!-- Visually Hidden Preheader Text : END -->
+
+<!--    
+    Set the email width. Defined in two places:
+    1. max-width for all clients except Desktop Windows Outlook, allowing the email to squish on narrow but never go wider than 680px.
+    2. MSO tags for Desktop Windows Outlook enforce a 680px width.
+    Note: The Fluid and Responsive templates have a different width (600px). The hybrid grid is more "fragile", and I&apos;ve found that 680px is a good width. Change with caution.  
+-->
+<div style="max-width: 680px; margin: auto;">
+    <!--[if mso]>
+    <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="680" align="center">
+    <tr>
+    <td>
+    <![endif]-->
+
+    <!-- Email Header : BEGIN -->
+    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;">
+        <tr>
+            <td style="padding: 20px 0; text-align: center">
+                <img src="http://mjcs.my/images/headerLogo.png" width="200" height="50" alt="alt_text" border="0" style="font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;">
+            </td>
+        </tr>
+    </table>
+    <!-- Email Header : END -->
+    
+    <!-- Email Body : BEGIN -->
+    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;">
+        
+        <!-- Hero Image, Flush : BEGIN -->
+        <tr>
+            <td bgcolor="#ffffff">
+                <img src="http://www.mjcs.my/assets/images/home-bg.jpg" width="680" height="" alt="alt_text" border="0" align="center" class="fluid" style="width: 100%; max-width: 680px; background: #dddddd; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;">
+            </td>
+        </tr>
+        <!-- Hero Image, Flush : END -->
+
+        <!-- 1 Column Text + Button : BEGIN -->
+        <tr>
+            <td bgcolor="#ffffff">
+                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                    <tr>
+                        <td style="padding: 40px; text-align: center; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;">
+                            Thank you for sending us service request!<br>
+                            We will provide our quotation of service as soon as posible.
+                            <br><br>
+                            <!-- Button : Begin -->
+                            <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto">
+                                <tr>
+                                    <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                        <a href="http://www.mjcs.my" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                            &nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#ffffff">Go to MJCS.MY</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- Button : END -->
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <!-- 1 Column Text + Button : BEGIN -->
+
+        <!-- Clear Spacer : BEGIN -->
+        <tr>
+            <td height="40" style="font-size: 0; line-height: 0;">
+                &nbsp;
+            </td>
+        </tr>
+        <!-- Clear Spacer : END -->
+
+    </table>
+    <!-- Email Body : END -->
+  
+    <!-- Email Footer : BEGIN -->
+    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;">
+        <tr>
+            <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #888888;">
+                MJ Cleaning Solutions Sdn Bhd (1129136-A)<br>
+                <span class="mobile-link--footer">No. 64-1, Jalan Mutiara Barat 6,<br> Taman Taynton View,<br> Cheras 56000 Kuala Lumpur<br>
+                </span><br><span class="mobile-link--footer">customerservice@mjcs.my</span>
+            </td>
+        </tr>
+    </table>
+    <!-- Email Footer : END -->
+
+    <!--[if mso]>
+    </td>
+    </tr>
+    </table>
+    <![endif]-->
+</div>
+</center>
+';
 ?>
--- test.php
+++ test.php
@@ -1,68 +1,15 @@
 <?php
-$first_name="";
-if(isset($_POST["first_name"])&&!empty($_POST["first_name"])){
-	$first_name=$_POST["first_name"];
-}
-$contact="";
-if(isset($_POST["contact"])&&!empty($_POST["contact"])){
-	$contact=$_POST["contact"];
-}
-$email="";
-if(isset($_POST["email"])&&!empty($_POST["email"])){
-	$email=$_POST["email"];
-}
-$date="";
-if(isset($_POST["date"])&&!empty($_POST["date"])){
-	$date=$_POST["date"];
-}
-$time="";
-if(isset($_POST["time"])&&!empty($_POST["time"])){
-	$time=$_POST["time"];
-}
-$service_area="";
-if(isset($_POST["service_area"])&&!empty($_POST["service_area"])){
-	$service_area=$_POST["service_area"];
-}
-$service_state="";
-if(isset($_POST["service_state"])&&!empty($_POST["service_state"])){
-	$service_state=$_POST["service_state"];
-}
-
-$to = $admin_email;
-$subject = "Incoming Enquiry from Website";
-
-$message = "
-<html>
-<head>
-<title>Incoming Enquiry</title>
-</head>
-<body>
-<p>You have new service request from website.</p>
-
-<b>Name: </b> ".$first_name."<br>
-<b>Contact: </b> ".$contact."<br>
-<b>Email: </b> ".$email."
-<br><br><b>Service Request Details</b><br>
-<b>Date: </b> ".$date."<br>
-<b>Time: </b> ".$time."<br>
-<b>Place: </b> ".$service_area."<br>
-<b>State: </b> ".$service_state."<br>
-
-</body>
-</html>
-";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <noreply@mjcs.my>' . "\r\n";
-
-mail($to,$subject,$message,$headers);
-mail("brandon@kkbuddy.com",$subject,$message,$headers);
-
-$subject="MJCS.MY - Thanks for sending in inquery.";
+session_start();
+/*
+TItle: WebbyCMS
+Authoer: Brandon Chong
+Version: 1.0
+*/
+?>
+<?php require_once('controller/conn.php'); ?>
+<?php require_once('controller/functions.php'); ?>
+<?php require_once('controller/common.php'); ?>
+<?php
 $message='
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -215,8 +162,5 @@ $message='
 </body>
 </html>
 ';
-
-mail($email,$subject,$message,$headers);
-
 ?>
-<script>window.location="?done=1";</script>
+<?=$message?>
